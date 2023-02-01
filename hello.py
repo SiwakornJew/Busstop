d72951832206ad8a6d0488423eadeb80f20dd39b
Siwakorn Noisundod <69206848+SiwakornJew@users.noreply.github.com>
2023-02-01 14:22:58 +0700

    'Api'

---
+++ hello.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, json, Response
+
+app = Flask(__name__)
+
+
+@app.route("/find", methods=['GET'])
+def hello_world():
+    try:
+        json_data = request.get_json()
+        if json_data['app_key'] != "1256953732aD24v":
+            print(json_data['app_key'])
+            return Response(json_data['app_key'] + "is not app key",
+                            status=404)
+        da = open('mockdat.json')
+        data = json.load(da)
+        return Response(data + "ok", status=200)
+    except Exception as error:
+        return Response("Data is not found", status=404)
