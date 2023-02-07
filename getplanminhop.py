c04717b1eefa347963b102b8549dc13cad540678
Siwakorn Noisundod <69206848+SiwakornJew@users.noreply.github.com>
2023-02-07 13:15:58 +0700

    update

--- getplanminhop.py
+++ getplanminhop.py
@@ -1,20 +1,18 @@
 from flask import Flask, request, json, Response
 
 app = Flask(__name__)
-
-
-@app.route("/getplanminhop", methods=['GET'])
-def hello_world():
+def getplanminhop():
     try:
-
-        print(data)
+        da = open('mockdata.json')
+        data = json.load(da)
         json_data = request.get_json()
         if json_data['app_key'] != "1256953732aD24v":
             print(json_data['app_key'])
             return Response(json_data['app_key'] + "is not app key",
                             status=404)
-        da = open('mockdata.json')
-        data = json.load(da)
         return data
     except Exception as error:
         return Response("Data is not found", status=404)
+
+@app.route("/getplanminhop", methods=['GET'])(getplanminhop)
+
