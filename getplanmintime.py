4838cb352f937a98df8c29970aa054e95d29ebc9
Siwakorn Noisundod <69206848+SiwakornJew@users.noreply.github.com>
2023-02-13 09:00:29 +0700

    update

--- getplanmintime.py
+++ getplanmintime.py
@@ -3,7 +3,7 @@ from flask import Flask, request, json, Response
 app = Flask(__name__)
 
 
-@app.route("/getplanminhop", methods=['GET'])
+@app.route("/getplanmintime", methods=['GET'])
 def getplanminhop():
     try:
         da = open('mockdata.json')
